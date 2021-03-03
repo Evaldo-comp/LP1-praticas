293fef531e72004e829a58570981522b1d8343d7
Carolinas-dsantos <carolin.silva@aluno.uece.br>
2021-03-03 20:39:36 -0300

    primeiro

---
+++ aula2ex1.py
@@ -0,0 +1,24 @@
+nome = input("Seu nome: ")
+idade = int(input("Sua idade: "))
+estadocivil = input("Seu estado civil: ")
+genero = input("Seu genero é: ")
+
+def nome (n):
+    return n
+
+def idade (i):
+    return i 
+
+def estadocivil (ec):
+    return ec
+
+def genero (g):
+    return g 
+
+    print(nome (n))
+
+    print(idade(i))
+
+    print(estadocivil(ec))
+
+    print(genero(g))
---
+++ convert_idade_minex2.py
@@ -0,0 +1,10 @@
+def idade_em_min (i):
+    dia = i *365
+    horas = dias *24
+    minutos = horas *60
+    segundos= minutos *60
+  
+    return minutos
+    minutos = idade_em_min('21')
+
+    print("Os minutos correspodentes a sua idade é: ")
